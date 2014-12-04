b8dc3f4ff5197800338f5472a36b251cf7929fbe
Nick Sullivan <nick@changetip.com>
2014-12-04 15:50:56 -0800

    Initial setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='changetip',
+      version='0.0.1',
+      description='ChangeTip helper library',
+      url='http://github.com/storborg/funniest',
+      author='ChangeCoin, Inc.',
+      author_email='oss@changecoin.com',
+      license='MIT',
+      packages=['bots'])
