f58693e86ea7b925f276324cbe94d7ded2552f3b
Nick Sullivan <nick@changetip.com>
2014-12-05 03:51:53 -0800

    More complete setup metadata

--- setup.py
+++ setup.py
@@ -4,7 +4,17 @@ setup(name='changetip',
       version='0.0.1',
       description='ChangeTip helper library',
       url='http://github.com/storborg/funniest',
+      long_description=open("README.md").read(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Text Processing :: Linguistic',
+      ],
       author='ChangeCoin, Inc.',
       author_email='oss@changecoin.com',
       license='MIT',
-      packages=['bots'])
+      install_requires=['requests'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      packages=['changetip'])
