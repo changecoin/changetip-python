6c616ca5380fb675a37bcede498d65ff9ec6fc37
Nick Sullivan <nick@sullivanflock.com>
2014-12-17 22:40:37 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='changetip',
-    version='0.1.1',
+    version='0.2.0',
     description='ChangeTip helper library',
     url='https://github.com/changecoin/changetip-python',
     classifiers=[
