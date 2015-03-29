b1de8190ebab6ab930258bcb80461c4e1ea12f5d
jim lyndon <jimlyndon@live.com>
2015-03-29 15:37:04 -0300

    fix bad dict lookup on error

--- changetip/bots/base.py
+++ changetip/bots/base.py
@@ -68,7 +68,7 @@ class BaseBot(object):
             "meta_json": meta,
         })
         response = requests.post(self.get_api_url("/tips/"), data=data, headers={'content-type': 'application/json'})
-        if response.headers["Content-Type"] == "application/json":
+        if response.headers.get("Content-Type", None) == "application/json":
             out = response.json()
             out["state"] = response.reason.lower()
             return out
