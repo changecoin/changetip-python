daa6e2f41712eb58d16c537af34f9f0a1b74a14e
Nick Sullivan <nick@sullivanflock.com>
2015-10-06 19:12:14 -0700

    0.3.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='changetip',
-    version='0.3.5',
+    version='0.3.6',
     description='ChangeTip helper library',
     url='https://github.com/changecoin/changetip-python',
     classifiers=[
